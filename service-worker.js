d735768e4ea33d2403bb4ed045beb3c6a0dea582
muslihudin <hikariroger@gmail.com>
2020-11-03 22:43:01 +0700

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -38,7 +38,41 @@ workbox.routing.registerRoute(
   })
 );
 
+self.addEventListener("fetch", function (event) {
+  var base_url = "https://api.football-data.org/v2/";
 
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function (response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        })
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request, { ignoreSearch: true }).then(function (response) {
+        return response || fetch(event.request);
+      })
+    )
+  }
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('push', function (event) {
     var body;
