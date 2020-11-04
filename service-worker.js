cfaa9b9c4217f7e0d7dc2eac7aeba0475fcfa928
muslihudin <hikariroger@gmail.com>
2020-11-04 11:38:48 +0700

    update service worker fix

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ workbox.precaching.precacheAndRoute([
 ]);
 
 workbox.routing.registerRoute(
-  /index\.html/,
+  /.*\.html/,
   workbox.strategies.networkFirst({
     cacheName: 'workbox:html',
   })
@@ -52,14 +52,48 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-  "api.football-data.org/v2/",
+  new RegExp("https://api.football-data.org/v2/"),
   workbox.strategies.networkFirst({
     cacheName: 'workbox:api'
   }),
   'GET'
 );
 
+self.addEventListener("fetch", function (event) {
+  var base_url = "https://api.football-data.org/v2/";
 
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function (response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        })
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request, { ignoreSearch: true }).then(function (response) {
+        return response || fetch(event.request);
+      })
+    )
+  }
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('push', function (event) {
     var body;
